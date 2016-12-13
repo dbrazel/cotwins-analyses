48818ff4eabe8f7455eb5b7823b829d33f350230
David Brazel <dbrazel@gmail.com>
2016-12-13 00:06:58 -0700

    Added scripts to create a mapping of Colorado IDs to Michigan IDs

---
+++ src/data/make_id_mapping_csvs.R
@@ -0,0 +1,18 @@
+# Create the CSVs required for the python script to generate the ID mapping
+
+library(readr)
+library(dplyr)
+
+paper <- read_rds(file.path("data", "raw", "Robin_paper-entry_12-6-16.rds"))
+users <- read_rds(file.path("data", "raw", "Michigan_DB_users_12_12_16.rds"))
+
+# Rearrange paper to produce one row per twin pair
+paper <- paper %>% select(ID = ID1, bestzygos)
+paper2 <- tbl_df(data.frame(c(paper[seq(1, nrow(paper), by = 2), "ID"]), 
+                 c(paper[seq(2, nrow(paper), by = 2), "ID"]),
+                 "bestzygos" = c(paper[seq(1, nrow(paper), by = 2), "bestzygos"]),
+                 stringsAsFactors = FALSE))
+paper2 <- select(paper2, T1 = ID, T2 = `ID.1`, bestzygos)
+
+write_csv(paper2, file.path("data", "raw", "Robin_paper-entry_12-6-16.csv"))
+write_csv(users, file.path("data", "raw", "Michigan_DB_users_12_12_16.csv"))
