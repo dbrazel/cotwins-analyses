7e451f58e5baf466c86b85b7daf3ee719653558e
David Brazel <dbrazel@gmail.com>
2017-05-11 23:30:17 -0600

    Modified the PhenX diagnoses script to be resilient to missing data

--- src/data/get_phenx_diagnoses.R
+++ src/data/get_phenx_diagnoses.R
@@ -46,10 +46,10 @@ life_alc_legal <- phenx$PHXQ0084 == 1
 year_alc_legal <- life_alc_legal & (phenx$PHXQ0085 == 1)
 
 # Alcohol abuse diagnoses and symptom counts
-results$life_alc_abuse_count <- life_alc_failure + life_alc_hazard + life_alc_continue + life_alc_legal
+results$life_alc_abuse_count <- rowSums(tibble(life_alc_failure, life_alc_hazard, life_alc_continue, life_alc_legal), na.rm = T)
 results$life_alc_abuse <- results$life_alc_abuse_count >= 1
 
-results$year_alc_abuse_count  <- year_alc_failure + year_alc_hazard + year_alc_continue + year_alc_legal
+results$year_alc_abuse_count  <- rowSums(tibble(year_alc_failure, year_alc_hazard, year_alc_continue, year_alc_legal), na.rm = T)
 results$year_alc_abuse <- results$year_alc_abuse_count >= 1
 
 # Alcohol dependence symptoms
@@ -62,12 +62,12 @@ year_alc_quit <- life_alc_quit & ((phenx$PHXQ0022 == 1) | (phenx$PHXQ0024 == 1))
 life_alc_intend <- (phenx$PHXQ0025 == 1) | (phenx$PHXQ0027 == 1)
 year_alc_intend <- life_alc_intend & ((phenx$PHXQ0026 == 1) | (phenx$PHXQ0028 == 1))
 
-life_alc_withdrawal_count <- (phenx$PHXQ0031 == 1) + (phenx$PHXQ0033 == 1) + (phenx$PHXQ0035 == 1) + (phenx$PHXQ0037 == 1) + 
-	(phenx$PHXQ0039 == 1) + (phenx$PHXQ0041 == 1) + (phenx$PHXQ0043 == 1) + (phenx$PHXQ0045 == 1)
-year_alc_withdrawal_count <- (phenx$PHXQ0031 == 1 & phenx$PHXQ0032 == 1) + (phenx$PHXQ0033 == 1 & phenx$PHXQ0034 == 1) + 
-	(phenx$PHXQ0035 == 1 & phenx$PHXQ0036 == 1) + (phenx$PHXQ0037 == 1 & phenx$PHXQ0038 == 1) + 
-	(phenx$PHXQ0039 == 1 & phenx$PHXQ0040 == 1) + (phenx$PHXQ0041 == 1 & phenx$PHXQ0042 == 1) + 
-	(phenx$PHXQ0043 == 1 & phenx$PHXQ0044 == 1) + (phenx$PHXQ0045 == 1 & phenx$PHXQ0046 == 1)
+life_alc_withdrawal_count <- rowSums(tibble((phenx$PHXQ0031 == 1), (phenx$PHXQ0033 == 1), (phenx$PHXQ0035 == 1), (phenx$PHXQ0037 == 1), 
+	(phenx$PHXQ0039 == 1), (phenx$PHXQ0041 == 1), (phenx$PHXQ0043 == 1), (phenx$PHXQ0045 == 1)), na.rm = T)
+year_alc_withdrawal_count <- rowSums(tibble((phenx$PHXQ0031 == 1 & phenx$PHXQ0032 == 1), (phenx$PHXQ0033 == 1 & phenx$PHXQ0034 == 1), 
+	(phenx$PHXQ0035 == 1 & phenx$PHXQ0036 == 1), (phenx$PHXQ0037 == 1 & phenx$PHXQ0038 == 1), 
+	(phenx$PHXQ0039 == 1 & phenx$PHXQ0040 == 1), (phenx$PHXQ0041 == 1 & phenx$PHXQ0042 == 1), 
+	(phenx$PHXQ0043 == 1 & phenx$PHXQ0044 == 1), (phenx$PHXQ0045 == 1 & phenx$PHXQ0046 == 1)), na.rm = T)
 
 life_alc_withdrawal <- ((life_alc_withdrawal_count >= 2) & (life_alc_withdrawal_count <= 8)) |
 	(phenx$PHXQ0050 == 1) | (phenx$PHXQ0052 == 1)
@@ -84,26 +84,28 @@ life_alc_problems <- (phenx$PHXQ0062 == 1) | (phenx$PHXQ0064 == 1) | (phenx$PHXQ
 year_alc_problems <- life_alc_problems & ((phenx$PHXQ0063 == 1) | (phenx$PHXQ0065 == 1) | (phenx$PHXQ0067 == 1))
 
 # Alcohol dependence diagnoses and symptom counts
-results$life_alc_depend_count <- life_alc_tolerance + life_alc_quit + life_alc_intend + 
-	life_alc_withdrawal + life_alc_time + life_alc_giveup + life_alc_problems
+results$life_alc_depend_count <- rowSums(tibble(life_alc_tolerance, life_alc_quit, life_alc_intend, 
+	life_alc_withdrawal, life_alc_time, life_alc_giveup, life_alc_problems), na.rm = T)
 results$life_alc_depend <- results$life_alc_depend_count >= 3
 
-results$year_alc_depend_count <- year_alc_tolerance + year_alc_quit + year_alc_intend + 
-	year_alc_withdrawal + year_alc_time + year_alc_giveup + year_alc_problems
+results$year_alc_depend_count <- rowSums(tibble(year_alc_tolerance, year_alc_quit, year_alc_intend, 
+	year_alc_withdrawal, year_alc_time, year_alc_giveup, year_alc_problems), na.rm = T)
 results$year_alc_depend <- results$year_alc_depend_count >= 3
 
-# TODO Current cigarette dependence
+# Current cigarette dependence
+
+
 
 # Tobacco dependence symptoms
 life_tob_tolerance <- (phenx$PHXQ0151 == 1) | (phenx$PHXQ0153 == 1)
 year_tob_tolerance <- life_tob_tolerance & ((phenx$PHXQ0152 == 1) | (phenx$PHXQ0154 == 1))
 
-life_tob_withdrawal_count <- (phenx$PHXQ0127 == 1) + (phenx$PHXQ0129 == 1) + (phenx$PHXQ0131 == 1) + (phenx$PHXQ0133 == 1) + 
-	(phenx$PHXQ0135 == 1) + (phenx$PHXQ0137 == 1) + (phenx$PHXQ0139 == 1) + (phenx$PHXQ0141 == 1)
-year_tob_withdrawal_count <- (phenx$PHXQ0127 == 1 & phenx$PHXQ0128 == 1) + (phenx$PHXQ0129 == 1 & phenx$PHXQ0130 == 1) + 
-	(phenx$PHXQ0131 == 1 & phenx$PHXQ0132 == 1) + (phenx$PHXQ0133 == 1 & phenx$PHXQ0134 == 1) + 
-	(phenx$PHXQ0135 == 1 & phenx$PHXQ0136 == 1) + (phenx$PHXQ0137 == 1 & phenx$PHXQ0138 == 1) + 
-	(phenx$PHXQ0139 == 1 & phenx$PHXQ0140 == 1) + (phenx$PHXQ0141 == 1 & phenx$PHXQ0142 == 1)
+life_tob_withdrawal_count <- rowSums(tibble((phenx$PHXQ0127 == 1), (phenx$PHXQ0129 == 1), (phenx$PHXQ0131 == 1), (phenx$PHXQ0133 == 1), 
+	(phenx$PHXQ0135 == 1), (phenx$PHXQ0137 == 1), (phenx$PHXQ0139 == 1), (phenx$PHXQ0141 == 1)), na.rm = T)
+year_tob_withdrawal_count <- rowSums(tibble((phenx$PHXQ0127 == 1 & phenx$PHXQ0128 == 1), (phenx$PHXQ0129 == 1 & phenx$PHXQ0130 == 1), 
+	(phenx$PHXQ0131 == 1 & phenx$PHXQ0132 == 1), (phenx$PHXQ0133 == 1 & phenx$PHXQ0134 == 1), 
+	(phenx$PHXQ0135 == 1 & phenx$PHXQ0136 == 1), (phenx$PHXQ0137 == 1 & phenx$PHXQ0138 == 1), 
+	(phenx$PHXQ0139 == 1 & phenx$PHXQ0140 == 1), (phenx$PHXQ0141 == 1 & phenx$PHXQ0142 == 1)), na.rm = T)
 
 life_tob_withdrawal <- (life_tob_withdrawal_count > 3) | (phenx$PHXQ0144 == 1) | (phenx$PHXQ0145 == 1) | 
 	(phenx$PHXQ0147 == 1) | (phenx$PHXQ0149 == 1)
@@ -126,12 +128,12 @@ life_tob_problems <- (phenx$PHXQ0119 == 1) | (phenx$PHXQ0157 == 1)
 year_tob_problems <- life_tob_problems & ((phenx$PHXQ0120 == 1) | (phenx$PHXQ0158 == 1))
 
 # Tobacco dependence diagnoses and symptom counts
-results$life_tob_depend_count <- life_tob_tolerance + life_tob_withdrawal + life_tob_intend + life_tob_quit +
-	life_tob_giveup + life_tob_time + life_tob_problems
+results$life_tob_depend_count <- rowSums(tibble(life_tob_tolerance, life_tob_withdrawal, life_tob_intend, life_tob_quit,
+	life_tob_giveup, life_tob_time, life_tob_problems), na.rm = T)
 results$life_tob_depend <- results$life_tob_depend_count >= 3
 
-results$year_tob_depend_count <- year_tob_tolerance + year_tob_withdrawal + year_tob_intend + year_tob_quit +
-	year_tob_giveup + year_tob_time + year_tob_problems
+results$year_tob_depend_count <- rowSums(tibble(year_tob_tolerance, year_tob_withdrawal, year_tob_intend, year_tob_quit,
+	year_tob_giveup, year_tob_time, year_tob_problems), na.rm = T)
 results$year_tob_depend <- results$year_tob_depend_count >= 3
 
 # Write out the results
