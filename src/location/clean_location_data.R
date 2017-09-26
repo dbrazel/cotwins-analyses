df06dfe3834963bd1ad47b5c3363f8385f745482
David Brazel <david.brazel@colorado.edu>
2017-09-25 19:35:59 -0600

    Updated the location cleaning script for the September 25, 2017 db dump

--- src/location/clean_location_data.R
+++ src/location/clean_location_data.R
@@ -2,12 +2,12 @@ library(readr)
 library(dplyr)
 library(lubridate)
 
-locations <- read_rds('data/raw/Michigan_DB_user_location_05_31_17.rds')
+locations <- read_rds('data/raw/Michigan_DB_user_location_09_25_17.rds')
 locations <- locations %>%
   select(creation_date:sample_timezone) %>%
   na.omit() %>%
   unique() %>%
   mutate(creation_date = ymd_hms(creation_date), sample_time = ymd_hms(sample_time))
-locations <- locations %>% filter(accuracy < 500, sample_time > ymd('2015-01-01'), sample_time < ymd('2017-06-01'))
+locations <- locations %>% filter(accuracy < 500, sample_time > ymd('2015-01-01'), sample_time < ymd('2017-09-25'))
 locations <- locations[!duplicated(locations[c('user_id', 'latitude', 'longitude', 'sample_time')]), ]
-write_rds(locations, 'data/processed/Michigan_DB_user_location_05_31_17_cleaned.rds')
+write_rds(locations, 'data/processed/Michigan_DB_user_location_09_25_17_cleaned.rds')
