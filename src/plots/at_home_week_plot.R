2f922be1cbed251802562455ed53e465b7caa045
David Brazel <dbrazel@users.noreply.github.com>
2017-11-28 01:54:37 -0700

    Wrote a script to plot time at home vs. day of week and hour of day

---
+++ src/plots/at_home_week_plot.R
@@ -0,0 +1,26 @@
+# A plot of fraction of points at home by day of week and hour of day
+
+library(readr)
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+library(forcats)
+library(viridis)
+
+locs <- read_rds("data/processed/at_home.rds")
+locs <- na.omit(locs)
+
+# Get local time and then extract day and hour
+locs %>%
+  mutate(DateTime = DateTime + minutes(sample_timezone)) %>%
+  mutate(weekday = wday(DateTime, label = T), hourofday = hour(DateTime)) %>%
+  mutate(hourofday = parse_factor(as.character(hourofday), as.character(seq(23, 0)))) %>%
+  group_by(weekday, hourofday) %>%
+  summarize(at_home_frac = sum(at_home) / n()) %>%
+  ggplot(aes(weekday, hourofday)) +
+  geom_tile(aes(fill = at_home_frac)) +
+  xlab("Day of week") +
+  ylab("Hour of day") +
+  scale_fill_viridis(name = "Fraction at home")
+
+ggsave("figs/at_home_week.pdf")
