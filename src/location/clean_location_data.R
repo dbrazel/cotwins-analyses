04be731957ca1b5d4a1305c4ce4bfa2e4a3ca2d1
David Brazel <david.brazel@colorado.edu>
2017-11-27 18:00:16 -0700

    Modified the location cleaning script to include only UTC points

--- src/location/clean_location_data.R
+++ src/location/clean_location_data.R
@@ -1,13 +1,34 @@
+# Clean the location data
+# Get rid of duplicate points and points that are missing a key field
+# Convert the time stamps to datetime objects
+# Require accuracy to be better than 500 meters and sample_time to be
+# within the possible range
+# Add an app_type column and select for points with timestamps in UTC
+
 library(readr)
 library(dplyr)
 library(lubridate)
 
 locations <- read_rds('data/raw/Michigan_DB_user_location_09_25_17.rds')
-locations <- locations %>%
-  select(creation_date:sample_timezone) %>%
-  na.omit() %>%
-  unique() %>%
-  mutate(creation_date = ymd_hms(creation_date), sample_time = ymd_hms(sample_time))
-locations <- locations %>% filter(accuracy < 500, sample_time > ymd('2015-01-01'), sample_time < ymd('2017-09-25'))
+user_info <- read_rds('data/raw/Michigan_DB_users_09_25_17.rds')
+user_info <- select(user_info, alternate_id, app_type)
+
 locations <- locations[!duplicated(locations[c('user_id', 'latitude', 'longitude', 'sample_time')]), ]
+
+locations <- locations %>%
+  select(id:timestamp_type) %>%
+  filter(
+    !is.na(longitude),
+    !is.na(latitude),
+    !is.na(creation_date),
+    !is.na(sample_time),
+    !is.na(user_id),
+    !is.na(accuracy)) %>%
+  mutate(creation_date = ymd_hms(creation_date), sample_time = ymd_hms(sample_time))
+
+locations <- locations %>% filter(accuracy < 500, sample_time > ymd('2015-01-01'), sample_time < ymd('2017-09-25'))
+
+locations <- left_join(locations, user_info, by = c('user_id' = 'alternate_id'))
+locations <- filter(locations, app_type == 'android' | timestamp_type == 'iOS_UTC')
+
 write_rds(locations, 'data/processed/Michigan_DB_user_location_09_25_17_cleaned.rds')
