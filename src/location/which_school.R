dfa3bfd5cc4e3e2d557287b929166b71bc34b59b
David Brazel <david.brazel@colorado.edu>
2017-11-30 22:17:10 -0700

    Wrote a script to find how many points each twin has at each school in Colorado

---
+++ src/location/which_school.R
@@ -0,0 +1,64 @@
+# For each twin and school, find how many standardized points
+# are within 100 meters
+
+library(readr)
+library(dplyr)
+library(geosphere)
+
+std_locs <- read_rds("data/processed/std_locations.rds")
+schools <- read_rds("data/processed/all_school_address.rds")
+
+# Use only non-missing points
+std_locs <- na.omit(std_locs)
+
+twins <- unique(std_locs$Colorado_ID)
+
+# Make an empty tibble to hold the results
+# One row per twin and school:
+# n_school_points - Number of standardized points
+#   within 100 meters of the school for the twin
+# total_points - Total number of standardized points for the twin
+# school_points_frac - n_school_points / total_points
+results <- tibble(
+  twin_id = character(),
+  school_id = character(),
+  latitude = numeric(),
+  longitude = numeric(),
+  n_school_points = integer(),
+  total_points = integer(),
+  school_points_frac = numeric()
+)
+
+# Loop over the twins
+for (twin in twins) {
+  # For each twin, get their locations and make a tibble to hold their results
+  subset <- filter(std_locs, Colorado_ID == twin)
+  twin_results <- tibble(
+    twin_id = twin,
+    school_id = schools$school_id,
+    latitude = schools$latitude,
+    longitude = schools$longitude,
+    n_school_points = NA,
+    total_points = NA,
+    school_points_frac = NA
+    )
+  
+  # Loop over each school and and find how many of the twin's points
+  # are within 100 meters of the school
+  for (i in 1:nrow(twin_results)) {
+    dists <- distCosine(
+      c(twin_results[[i, "longitude"]], twin_results[[i, "latitude"]]),
+      cbind(subset$longitude, subset$latitude)
+      )
+    twin_results[i, "n_school_points"] <- sum(dists <= 100)
+    twin_results[i, "total_points"] <- nrow(subset)
+    twin_results[i, "school_points_frac"] <-
+      twin_results[i, "n_school_points"] / twin_results[i, "total_points"]
+  }
+  
+  # Append the twin's results to the results tibble
+  results <- bind_rows(results, twin_results)
+  print(twin)
+}
+
+write_rds(results, "data/processed/twin_by_school.rds")
