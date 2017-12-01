2979176a110a71c6126c85b00ea84b3562def551
David Brazel <david.brazel@colorado.edu>
2017-12-01 13:22:06 -0700

    Adds a script to visualize the fraction of points at school using a calendar heatmap

---
+++ src/plots/school_frac_calendar.R
@@ -0,0 +1,25 @@
+# Visualize the fraction of points at school over the course of 2017,
+# using a calendar heatmap
+
+library(readr)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(ggTimeSeries)
+library(viridis)
+
+locs <- read_rds("data/processed/at_school.rds")
+
+# Up the font size
+theme_set(theme_gray(base_size = 16))
+
+locs %>%
+  group_by(date = as_date(DateTime)) %>%
+  summarize(school_frac = sum(at_school) / n()) %>%
+  filter(date >= ymd("2017-01-01")) %>%
+  ggplot_calendar_heatmap('date', 'school_frac') +
+  xlab(NULL) +
+  ylab(NULL) +
+  scale_fill_viridis(name = "Fraction at school")
+
+ggsave("figs/school_frac_calendar.pdf", width = 10.1, height = 6.8)
