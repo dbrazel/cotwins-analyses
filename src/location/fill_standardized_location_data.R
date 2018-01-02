3b497bf4e4ea293fdebc2ed0f5cdd2d9a4119723
David Brazel <dbrazel@users.noreply.github.com>
2018-01-01 21:59:03 -0500

    Modified the script to be more efficient but not efficient enough

--- src/location/fill_standardized_location_data.R
+++ src/location/fill_standardized_location_data.R
@@ -5,7 +5,7 @@ library(readr)
 library(dplyr)
 library(lubridate)
 
-std_locs <- read_rds("data/processed/std_locations.rds")
+std_locs <- read_rds('data/processed/std_locations.rds')
 
 # Add a column to hold the original datetime a row had before it was copied
 std_locs["orig_datetime"] <- std_locs$DateTime
@@ -13,24 +13,32 @@ std_locs["orig_datetime"] <- std_locs$DateTime
 # Define how far back to search for a point
 time_valid <- hours(12)
 
-# If a row is missing for an iOS user and that twin has a sufficiently
-# recent point, copy it
+# Order the points by user id and then ascending datetime
+std_locs <- arrange(std_locs, Colorado_ID, DateTime)
+
+to_be_copied <- c(
+  'latitude',
+  'longitude',
+  'id',
+  'accuracy',
+  'sample_timezone',
+  'app_type',
+  'orig_datetime'
+)
+
+# If the row is missing and the previous row is from the same iOS user and is
+# not missing, and the difference between current row DateTime and previous row
+# orig_datetime is less than or equal to time_valid, copy the previous row to
+# the current row
 for (i in 1:nrow(std_locs)) {
-  if (is.na(std_locs[[i, "id"]])) {
-    valid_rows <- filter(
-      std_locs,
-      Colorado_ID == std_locs[[i, "Colorado_ID"]],
-      orig_datetime >= std_locs[[i, "DateTime"]] - time_valid,
-      orig_datetime < std_locs[[i, "DateTime"]],
-      !is.na(std_locs[[i, "id"]]),
-      app_type == "iOS"
-      ) %>%
-      arrange(desc(DateTime))
-    
-    # Go to the next row 
-    if (nrow(valid_rows) == 0) next()
-    
-    std_locs[i, "latitude":"app_type"] <- valid_rows[1, "latitude":"app_type"]
-    std_locs[i, "orig_datetime"] <- valid_rows[1, "orig_datetime"]
+  if (i == 1) next()
+  if (
+    is.na(std_locs[[i, 'id']]) &
+    !is.na(std_locs[[i - 1, 'id']]) &
+    std_locs[[i, 'Colorado_ID']] == std_locs[[i - 1, 'Colorado_ID']] &
+    std_locs[[i - 1, 'app_type']] == 'ios' &
+    std_locs[[i, 'DateTime']] - time_valid <= std_locs[[i - 1, 'orig_datetime']]
+  ) {
+    std_locs[i, to_be_copied] <- std_locs[i - 1, to_be_copied]
   }
 }
