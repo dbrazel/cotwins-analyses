2db4b5d7a4f4e1da2ac748009d22444c7c52ab78
David Brazel <dbrazel@users.noreply.github.com>
2018-01-02 00:37:39 -0500

    Switched to data.table to make the row modification faster
    When modifying a data frame or tibble, R will create multiple deep copies which is quite slow. Data.table implements updates in-place with the := operator which are much faster.

--- src/location/fill_standardized_location_data.R
+++ src/location/fill_standardized_location_data.R
@@ -16,22 +16,18 @@ time_valid <- hours(12)
 # Order the points by user id and then ascending datetime
 std_locs <- arrange(std_locs, Colorado_ID, DateTime)
 
-to_be_copied <- c(
-  'latitude',
-  'longitude',
-  'id',
-  'accuracy',
-  'sample_timezone',
-  'app_type',
-  'orig_datetime'
-)
+# Use data.table for its update in-place functionality - using tibbles or dfs
+# will cause R to create deep copies which is slow
+std_locs <- data.table::as.data.table(std_locs)
 
 # If the row is missing and the previous row is from the same iOS user and is
 # not missing, and the difference between current row DateTime and previous row
 # orig_datetime is less than or equal to time_valid, copy the previous row to
 # the current row
 for (i in 1:nrow(std_locs)) {
+  if (i %% 100 == 0) print(i)
   if (i == 1) next()
+  
   if (
     is.na(std_locs[[i, 'id']]) &
     !is.na(std_locs[[i - 1, 'id']]) &
@@ -39,6 +35,15 @@ for (i in 1:nrow(std_locs)) {
     std_locs[[i - 1, 'app_type']] == 'ios' &
     std_locs[[i, 'DateTime']] - time_valid <= std_locs[[i - 1, 'orig_datetime']]
   ) {
-    std_locs[i, to_be_copied] <- std_locs[i - 1, to_be_copied]
+    std_locs[i, 3 := std_locs[i - 1, 3]]
+    std_locs[i, 4 := std_locs[i - 1, 4]]
+    std_locs[i, 5 := std_locs[i - 1, 5]]
+    std_locs[i, 6 := std_locs[i - 1, 6]]
+    std_locs[i, 7 := std_locs[i - 1, 7]]
+    std_locs[i, 8 := std_locs[i - 1, 8]]
+    std_locs[i, 9 := std_locs[i - 1, 9]]
   }
 }
+
+std_locs <- as_tibble(std_locs)
+write_rds('data/processed/std_locations_filled.rds')
