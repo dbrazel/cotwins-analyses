b5fa8ca48ce02dc365d0f7690b1cef222d17ab8d
David Brazel <david.brazel@colorado.edu>
2018-01-30 15:43:08 -0700

    Updated the downtown denver plot script to produce an animated gif as well as the static plot

--- src/plots/downtown_denver_plot.R
+++ src/plots/downtown_denver_plot.R
@@ -1,18 +1,30 @@
+# Make a static plot and an animated GIF of the locations in downtown Denver
+
 library(readr)
 library(ggplot2)
 library(dplyr)
+library(lubridate)
+library(gganimate)
 
-locs <- read_rds("data/processed/Michigan_DB_user_location_09_25_17_cleaned.rds")
+locs <- read_rds(
+  "data/processed/Michigan_DB_user_location_09_25_17_cleaned.rds"
+  )
 
 downtown_locs <- locs %>%
   filter(
     latitude < 39.753696,
     longitude > -105.005600, 
     latitude > 39.740365,
-    longitude < -104.986696)
+    longitude < -104.986696) %>%
+  mutate(
+    sample_hour = hour(sample_time + minutes(sample_timezone))
+    )
 
 p <- 
-  ggplot(downtown_locs, aes(longitude, latitude)) +
+  ggplot(
+    downtown_locs,
+    aes(longitude, latitude, color = user_id, frame = sample_hour)
+    ) +
   coord_map() +
   geom_point(alpha = 0.5) +
   theme(
@@ -28,3 +40,5 @@ ggsave(
   width = 10,
   height = 10,
   units = "in")
+
+gganimate(p, "figs/downtown_denver.gif")
