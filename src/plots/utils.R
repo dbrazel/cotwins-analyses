cfd91d714e3cb1007a5426b2233d548149283252
David Brazel <dbrazel@users.noreply.github.com>
2018-03-10 23:40:23 -0700

    Adding a useful plotting command

--- src/plots/utils.R
+++ src/plots/utils.R
@@ -1,5 +1,12 @@
 # Useful functions for plotting
 
+# Look at the sub use trajectories of 20 twins
+sub_use %>%
+  filter(user_id %in% sample(unique(sub_use$user_id), 20)) %>%
+  ggplot(aes(test_age, drinks_per_week, group = user_id, color = user_id)) +
+  geom_smooth(se = F, alpha = 0.5) +
+  guides(color = F)
+
 # Plot fraction of location points at home,
 # over time, for a twin, using plotly
 at_home_plot <- function (twin, at_home_sum) {
