ad370440682dc85203ab5d171c1b959132231bcf
David Brazel <dbrazel@users.noreply.github.com>
2018-03-14 00:24:18 -0600

    Multivariate growth model of DPW and time at home at night

--- src/models/at_home_dpw_model.R
+++ src/models/at_home_dpw_model.R
@@ -2,7 +2,7 @@ library(readr)
 library(dplyr)
 library(tidyr)
 library(lubridate)
-library(lme4)
+library(nlme)
 
 sub_use <- read_rds("data/processed/remote_substance_use.rds")
 twin_info <- read_rds("data/processed/Robin_paper-entry_2-22-17_cleaned.rds") %>%
@@ -39,6 +39,9 @@ sub_use <- mutate(
   sex = Sex1 - 1
 )
 
+# Time at home isn't meaningful after age 18 or so
+#sub_use <- filter(sub_use, test_age <= 1.5)
+
 # If the twin didn't use alcohol that week, set the phenos to 0
 sub_use$alc_quantity_drinks_per_day[!sub_use$any_substance_use] <- 0
 sub_use$alc_quantity_drinks_per_day[!sub_use$alc_use] <- 0
@@ -72,3 +75,32 @@ for (i in 1:nrow(sub_use)) {
   sub_use[i, "home_frac"] <- sum(subset$at_home) / nrow(subset)
   print(i)
 }
+
+sub_use <- select(sub_use, user_id, family, test_age, drinks_per_week, sex, home_frac, home_n)
+sub_use <- na.omit(sub_use)
+
+# Get rid of subjects with very few responses
+valid_ids <- sub_use %>%
+  group_by(user_id) %>%
+  summarize(N = n()) %>%
+  filter(N > 5)
+
+sub_use <- filter(sub_use, user_id %in% valid_ids$user_id)
+
+# Convert to a single variable, dummy-coded
+dpw <- select(sub_use, user_id, family, var = drinks_per_week, test_age)
+dpw["d_dpw"] <- 1
+dpw["d_home"] <- 0
+dpw["grp"] <- "dpw"
+
+home <- select(sub_use, user_id, family, var = home_frac, test_age)
+home["d_dpw"] <- 0
+home["d_home"] <- 1
+home["grp"] <- "home"
+
+multivariate = rbind(dpw, home)
+
+multi_model <- nlme(var ~ d_dpw * (b_1i + b_2i * test_age) + d_home * (h_1i + h_2i * test_age), data = multivariate, fixed = b_1i + b_2i + h_1i + h_2i ~ 1, random = b_1i + b_2i + h_1i + h_2i ~ 1, group = ~user_id, start = c(0, 0.01, 0, 0.01), weights = varIdent(c(home = 0.5), form = ~1|grp), na.action = na.omit)
+multi_model_twin <- nlme(var ~ d_dpw * (b_1i + b_2i * test_age) + d_home * (h_1i + h_2i * test_age), data = multivariate, fixed = b_1i + b_2i + h_1i + h_2i ~ 1, random = b_1i + b_2i + h_1i + h_2i ~ 1|family/user_id, start = c(0, 0.01, 0, 0.01), weights = varIdent(c(home = 0.5), form = ~1|grp), na.action = na.omit)
+
+write_rds(multi_model, "data/models/at_home_dpw_model.rds")
