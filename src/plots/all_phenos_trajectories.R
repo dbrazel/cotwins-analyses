72d368116c33ceb613126da3b8019ec65a9f6077
David Brazel <david.brazel@colorado.edu>
2018-05-28 00:00:40 -0600

    Consolidate the GAMM scripts

--- src/plots/all_phenos_trajectories.R
+++ src/plots/all_phenos_trajectories.R
@@ -4,18 +4,17 @@
 # k values chosen using gam.check and ?choose.k
 
 library(readr)
-library(ggplot2)
 library(dplyr)
-library(cowplot)
 library(gamm4)
 library(lubridate)
+library(cowplot)
 
 twin_info <- read_rds("data/processed/Robin_paper-entry_2-22-17_cleaned.rds") %>%
   haven::zap_formats() %>%
   haven::zap_labels()
 id_mapping_long <- read_csv("data/processed/id_mapping_long.csv", col_types = "ccc")
 
-# At Home
+# At Home and At School
 at_home <- read_rds("data/processed/at_home_unfiltered.rds")
 
 # Shift the locations to local time and restrict to 12 AM to 5 AM
@@ -40,21 +39,13 @@ at_home_pred <- predict(at_home_ml$gam, se.fit = T)
 at_home_plot_data <-
   tibble(
     test_age = at_home_data$test_age + 17,
-    home_frac = at_home_pred$fit,
-    home_frac_lower = at_home_pred$fit - 1.96 * at_home_pred$se.fit,
-    home_frac_upper = at_home_pred$fit + 1.96 * at_home_pred$se.fit
+    frac = at_home_pred$fit,
+    lower = at_home_pred$fit - 1.96 * at_home_pred$se.fit,
+    upper = at_home_pred$fit + 1.96 * at_home_pred$se.fit
   )
 
-at_home_plot <- ggplot(at_home_plot_data, aes(test_age, home_frac)) +
-  geom_ribbon(aes(ymax = home_frac_upper, ymin = home_frac_lower), alpha = 0.2) +
-  geom_line() +
-  xlab("Age in years") +
-  ylab("Predicted fraction of time at home") +
-  xlim(c(14, 20))
+at_home_plot_data$pheno <- "Home"
 
-save_plot("figs/at_home_trajectory.pdf", at_home_plot)
-
-# At School
 at_school <- read_rds("data/processed/at_school_unfiltered.rds")
 
 # We started getting more locations in November 2016. Let's restrict to there and
@@ -114,19 +105,93 @@ at_school_pred <- predict(at_school_ml$gam, se.fit = T)
 at_school_plot_data <-
   tibble(
     test_age = at_school_data$test_age + 17,
-    school_frac = at_school_pred$fit,
-    school_frac_lower = at_school_pred$fit - 1.96 * at_school_pred$se.fit,
-    school_frac_upper = at_school_pred$fit + 1.96 * at_school_pred$se.fit
+    frac = at_school_pred$fit,
+    lower = at_school_pred$fit - 1.96 * at_school_pred$se.fit,
+    upper = at_school_pred$fit + 1.96 * at_school_pred$se.fit
   )
 
-at_school_plot <- ggplot(at_school_plot_data, aes(test_age, school_frac)) +
-  geom_ribbon(aes(ymax = school_frac_upper, ymin = school_frac_lower), alpha = 0.2) +
-  geom_line() +
-  xlab("Age in years") +
-  ylab("Predicted fraction of time at school") +
-  xlim(c(14, 20))
+at_school_plot_data$pheno <- "School"
 
-save_plot("figs/at_school_trajectory.pdf", at_school_plot)
+home_school_plot_data <- bind_rows(at_home_plot_data, at_school_plot_data)
+home_school_plot_data$pheno <- factor(home_school_plot_data$pheno, levels = c("Home", "School"))
+
+home_school_plot <- ggplot(home_school_plot_data, aes(test_age, frac, fill = pheno, color = pheno)) +
+  geom_ribbon(aes(ymax = upper, ymin = lower), alpha = 0.2) +
+  geom_line() +
+  scale_color_brewer("", palette = "Set1") +
+  scale_fill_brewer("", palette = "Set1") +
+  labs(
+    x = "Age (years)",
+    y = "Predicted fraction of time"
+  ) +
+  theme(legend.direction = "horizontal", legend.position = c(0.55, 0.05)) +
+  xlim(14, 20)
+
+# Twin Distance
+tp_dists <- read_rds("data/processed/twin_distances.rds")
+
+tp_dists <-
+  left_join(tp_dists, id_mapping_long, by = c("tp_id" = "alternate_id"))
+tp_dists <- select(tp_dists,-bestzygos)
+tp_dists <- left_join(tp_dists, twin_info, by = c("SVID" = "ID1"))
+tp_dists <-
+  select(tp_dists, tp_id, zygosity, DateTime, distance, Birth_Date)
+tp_dists <-
+  mutate(tp_dists, age = as.numeric(as_date(DateTime) - Birth_Date) / 365)
+tp_dists$zygosity = factor(tp_dists$zygosity, levels = c("MZ", "DZ", "OS"))
+tp_dists <-
+  group_by(tp_dists,
+           tp_id,
+           d = day(DateTime),
+           m = month(DateTime),
+           y = year(DateTime)) %>%
+  summarise(
+    zygosity = first(zygosity),
+    distance = mean(distance),
+    age = mean(age)
+  )
+
+# Winsorize based on the distribution of distance and log transform
+tp_dists[tp_dists$distance > 2e5, "distance"] <- 2e5
+tp_dists <- mutate(tp_dists, distance = log10(distance + 1))
+
+dist_ml <- gamm4(
+  distance ~ zygosity + s(age, by = zygosity),
+  random = ~(1 | tp_id), data = tp_dists
+)
+
+dist_pred <- predict(dist_ml$gam, se.fit = T)
+
+dist_plot_data <-
+  tibble(
+    age = tp_dists$age,
+    zygosity = tp_dists$zygosity,
+    distance = dist_pred$fit,
+    dist_lower = dist_pred$fit - 1.96 * dist_pred$se.fit,
+    dist_upper = dist_pred$fit + 1.96 * dist_pred$se.fit
+  )
+
+dist_plot <- ggplot(dist_plot_data, aes(age, distance, fill = zygosity, color = zygosity)) +
+  geom_ribbon(aes(ymax = dist_upper, ymin = dist_lower), alpha = 0.2) +
+  geom_line() +
+  scale_color_brewer(palette = "Set1") +
+  scale_fill_brewer(palette = "Set1") +
+  labs(
+    x = "Age (years)",
+    y = "Predicted log10 of distance (meters)",
+    color = "Zygosity",
+    fill = "Zygosity"
+  ) +
+  theme(legend.direction = "horizontal", legend.position = c(0.35, 0.05)) +
+  scale_y_continuous(
+    breaks = c(3, 4, 5, 6),
+    labels = c(
+      "1 km",
+      "10 km",
+      "100 km",
+      "1,000 km")
+  ) +
+  xlim(14, 20)
 
 # Parental Monitoring
 par_mon <- read_rds("data/processed/remote_parents.rds") %>%
@@ -157,13 +222,11 @@ par_mon_plot_data <-
 par_mon_plot <- ggplot(par_mon_plot_data, aes(test_age, max_monitor_score)) +
   geom_ribbon(aes(ymax = max_monitor_score_upper, ymin = max_monitor_score_lower), alpha = 0.2) +
   geom_line() +
-  xlab("Age in years") +
+  xlab("Age (years)") +
   ylab("Predicted parental monitoring score") +
-  xlim(c(14, 20))
+  xlim(14, 20)
 
-save_plot("figs/par_mon_trajectory.pdf", par_mon_plot)
-
-# Alcohol
+# Substance Use
 dpw_data <- read_rds("data/models/dpw_data.rds")
 
 dpw_ml <- gamm4(drinks_per_week ~ s(test_age, k = 4), random = ~(1 | family/user_id), data = dpw_data)
@@ -171,21 +234,13 @@ dpw_pred <- predict(dpw_ml$gam, se.fit = T)
 dpw_plot_data <-
   tibble(
     test_age = dpw_data$test_age + 17,
-    dpw = dpw_pred$fit,
-    dpw_lower = dpw_pred$fit - 1.96 * dpw_pred$se.fit,
-    dpw_upper = dpw_pred$fit + 1.96 * dpw_pred$se.fit
+    use = dpw_pred$fit,
+    lower = dpw_pred$fit - 1.96 * dpw_pred$se.fit,
+    upper = dpw_pred$fit + 1.96 * dpw_pred$se.fit
   )
 
-dpw_plot <- ggplot(dpw_plot_data, aes(test_age, dpw)) +
-  geom_ribbon(aes(ymax = dpw_upper, ymin = dpw_lower), alpha = 0.2) +
-  geom_line() +
-  xlab("Age in years") +
-  ylab("Predicted log DPW") +
-  xlim(c(14, 20))
+dpw_plot_data$pheno <- "Alcohol"
 
-save_plot("figs/alcohol_trajectory.pdf", dpw_plot)
-
-# Marijuana
 mpw_data <- read_rds("data/models/mpw_data.rds")
 
 mpw_ml <- gamm4(mar_per_week ~ s(test_age, k = 5), random = ~(1 | family/user_id), data = mpw_data)
@@ -193,53 +248,58 @@ mpw_pred <- predict(mpw_ml$gam, se.fit = T)
 mpw_plot_data <-
   tibble(
     test_age = mpw_data$test_age + 17,
-    mpw = mpw_pred$fit,
-    mpw_lower = mpw_pred$fit - 1.96 * mpw_pred$se.fit,
-    mpw_upper = mpw_pred$fit + 1.96 * mpw_pred$se.fit
+    use = mpw_pred$fit,
+    lower = mpw_pred$fit - 1.96 * mpw_pred$se.fit,
+    upper = mpw_pred$fit + 1.96 * mpw_pred$se.fit
   )
 
-mpw_plot <- ggplot(mpw_plot_data, aes(test_age, mpw)) +
-  geom_ribbon(aes(ymax = mpw_upper, ymin = mpw_lower), alpha = 0.2) +
-  geom_line() +
-  xlab("Age in years") +
-  ylab("Predicted log MPW") +
-  xlim(c(14, 20))
+mpw_plot_data$pheno <- "Marijuana"
 
-save_plot("figs/marijuana_trajectory.pdf", mpw_plot)
-
-# E-Cigarettes
 ecig_data <- read_rds("data/models/ecig_data.rds")
 
-ecig_ml <- gamm4(puffs_per_week ~ s(test_age, k = 4), random = ~(1 | family/user_id), data = ecig_data)
+ecig_ml <- gamm4(puffs_per_week ~ s(test_age, k = 7), random = ~(1 | family/user_id), data = ecig_data)
 ecig_pred <- predict(ecig_ml$gam, se.fit = T)
 ecig_plot_data <-
   tibble(
     test_age = ecig_data$test_age + 17,
-    ppw = ecig_pred$fit,
-    ppw_lower = ecig_pred$fit - 1.96 * ecig_pred$se.fit,
-    ppw_upper = ecig_pred$fit + 1.96 * ecig_pred$se.fit
+    use = ecig_pred$fit,
+    lower = ecig_pred$fit - 1.96 * ecig_pred$se.fit,
+    upper = ecig_pred$fit + 1.96 * ecig_pred$se.fit
   )
 
-ecig_plot <- ggplot(ecig_plot_data, aes(test_age, ppw)) +
-  geom_ribbon(aes(ymax = ppw_upper, ymin = ppw_lower), alpha = 0.2) +
-  geom_line() +
-  xlab("Age in years") +
-  ylab("Predicted log PPW") +
-  xlim(c(14, 20))
+ecig_plot_data$pheno <- "E-Cigarettes"
 
-save_plot("figs/ecigarette_trajectory.pdf", ecig_plot)
+sub_use_plot_data <- bind_rows(dpw_plot_data, mpw_plot_data) %>% bind_rows(ecig_plot_data)
+sub_use_plot_data$pheno <- factor(sub_use_plot_data$pheno, levels = c("Alcohol", "Marijuana", "E-Cigarettes"))
+
+sub_use_plot <- ggplot(sub_use_plot_data, aes(test_age, use, fill = pheno, color = pheno)) +
+  geom_ribbon(aes(ymax = upper, ymin = lower), alpha = 0.2) +
+  geom_line() +
+  scale_color_brewer("", palette = "Set1") +
+  scale_fill_brewer("", palette = "Set1") +
+  labs(
+    x = "Age (years)",
+    y = "Predicted ln of substance use"
+  ) +
+  theme(legend.direction = "horizontal", legend.position = c(0.2, 0.05)) +
+  xlim(14, 20)
 
 # Combine the plots with aligned axes
 all_plot <-
   plot_grid(
-    at_home_plot,
-    at_school_plot,
+    dist_plot,
+    home_school_plot,
     par_mon_plot,
-    dpw_plot,
-    mpw_plot,
-    ecig_plot,
+    sub_use_plot,
     align = "hv",
-    labels = c("A", "B", "C", "D", "E", "F")
+    labels = c("A", "B", "C", "D"),
+    nrow = 2,
+    ncol = 2
   )
 
-save_plot("figs/all_phenos_trajectories.pdf", all_plot, ncol = 3, nrow = 2)
+save_plot(
+  "figs/all_phenos_trajectories.pdf",
+  all_plot,
+  base_aspect_ratio = 1.3,
+  base_height = 8
+  )
