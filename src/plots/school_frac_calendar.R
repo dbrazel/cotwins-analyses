9f185375de216399ec7fbd8cd9f0d326f0fc6fc8
David Brazel <dbrazel@users.noreply.github.com>
2018-06-11 11:44:30 -0600

    Update the calendar heatmap for time at school

--- src/plots/school_frac_calendar.R
+++ src/plots/school_frac_calendar.R
@@ -4,22 +4,29 @@
 library(readr)
 library(dplyr)
 library(lubridate)
-library(ggplot2)
+library(cowplot)
 library(ggTimeSeries)
 library(viridis)
 
 locs <- read_rds("data/processed/at_school.rds")
 
-# Up the font size
-theme_set(theme_gray(base_size = 16))
+twin_info <- read_rds("data/processed/Robin_paper-entry_2-22-17_cleaned.rds") %>%
+  haven::zap_formats() %>%
+  haven::zap_labels()
+id_mapping_long <- read_csv("data/processed/id_mapping_long.csv", col_types = "ccc")
 
-locs %>%
+# Shift to local time
+locs <- mutate(locs, DateTime = DateTime + minutes(sample_timezone)) %>%
+  filter(hour(DateTime) %in% c(8, 9, 10, 11, 12, 13, 14))
+
+plt <- locs %>%
   group_by(date = as_date(DateTime)) %>%
   summarize(school_frac = sum(at_school) / n()) %>%
   filter(date >= ymd("2017-01-01")) %>%
   ggplot_calendar_heatmap('date', 'school_frac') +
   xlab(NULL) +
   ylab(NULL) +
-  scale_fill_viridis(name = "Fraction at school")
+  scale_fill_viridis(name = "Fraction at school") +
+  facet_wrap(~Year, ncol = 1)
 
-ggsave("figs/school_frac_calendar.pdf", width = 10.1, height = 6.8)
+save_plot("figs/school_frac_calendar.pdf", plt, base_aspect_ratio = 2.5, base_height = 4)
